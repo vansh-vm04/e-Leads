d48b73d73b4b0f1e9249bdc4ab6cfb2123178b4c
Vansh <vanshmorwani1@gmail.com>
2025-09-13 17:59:39 +0530

    declared types for next auth

---
+++ @types/next-auth.ts
@@ -0,0 +1,22 @@
+import { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface User extends DefaultUser {
+    id: string;
+    role?: string;
+  }
+
+  interface Session {
+    user?: {
+      id: string;
+      role?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role?: string;
+  }
+}

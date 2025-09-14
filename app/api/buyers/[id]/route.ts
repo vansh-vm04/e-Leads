b197931261c1c1b1dcf717f4cb6ac2dcb64d2c18
Vansh <vanshmorwani1@gmail.com>
2025-09-14 18:21:09 +0530

    feat: completed buyer apis

---
+++ app/api/buyers/[id]/route.ts
@@ -0,0 +1,118 @@
+import { Prisma } from "@/app/generated/prisma";
+import { authOptions } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { BuyerSchema, BuyerType } from "@/lib/zod/schema";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const { data, updatedAt } = await req.json();
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.id) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  const userId = session.user.id;
+  try {
+    const parsed = BuyerSchema.safeParse(data);
+    if (!parsed.success) {
+      return NextResponse.json(
+        { errors: parsed.error.issues },
+        { status: 400 }
+      );
+    }
+
+    const buyerData: BuyerType = parsed.data;
+
+    const currentUserTime = new Date(updatedAt);
+
+    const existing = await prisma.buyer.findUnique({
+      where: { id },
+    });
+    if (!existing) {
+      return NextResponse.json({error: "Buyer not found" }, {status: 404} );
+    }
+
+    if (existing.ownerId !== userId) {
+      return NextResponse.json({ error: "Forbidden" },{status: 403});
+    }
+
+    const previousTime = new Date(existing.updatedAt);
+
+    if (previousTime.toISOString() !== currentUserTime.toISOString()) {
+      return NextResponse.json(
+        { status: 409, error: "Record changed, please refresh" },
+        { status: 409 }
+      );
+    }
+
+    const updated = await prisma.buyer.update({
+      where: { id },
+      data: { ...buyerData },
+    });
+
+    const diff: Record<string, { old: unknown; new: unknown }> = {};
+    for (const key in buyerData) {
+      if (
+        (existing as Prisma.BuyerGetPayload<object>)[key as keyof BuyerType] !==
+        (buyerData as BuyerType)[key as keyof BuyerType]
+      ) {
+        diff[key as keyof BuyerType] = {
+          old: (existing as Prisma.BuyerGetPayload<object>)[
+            key as keyof BuyerType
+          ],
+          new: (buyerData as BuyerType)[key as keyof BuyerType],
+        };
+      }
+    }
+
+    await prisma.buyerHistory.create({
+      data: {
+        buyerId: id,
+        changedBy: userId,
+        diff: diff as Prisma.InputJsonValue,
+      },
+    });
+
+    return NextResponse.json({ buyer: updated },{status: 200});
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Server error" },{status: 500});
+  }
+}
+
+export async function DELETE(
+    req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const session = await getServerSession(authOptions);
+  if (!session?.user?.id) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+  const userId = session.user.id;
+  try {
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+    if (!user) {
+      return NextResponse.json({ message: "User not found" },{status: 404});
+    }
+
+    const buyer = await prisma.buyer.findUnique({ where: { id } });
+
+    if (user?.id !== buyer?.ownerId) {
+      return NextResponse.json(
+        { message: "Anauthorized User" },
+        { status: 401 }
+      );
+    }
+    await prisma.buyerHistory.deleteMany({where:{buyerId: id}});
+    await prisma.buyer.delete({ where: { id } });
+    return NextResponse.json({ message: "Buyer deleted" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Unable to delete" }, { status: 500 });
+  }
+}
---
+++ app/api/buyers/route.ts
@@ -0,0 +1,119 @@
+import { authOptions } from "@/lib/auth";
+import prisma from "@/lib/db";
+import { type BuyerType, BuyerSchema } from "@/lib/zod/schema";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.id) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const { searchParams } = new URL(req.url);
+
+    const page = parseInt(searchParams.get("page") || "1", 10);
+    const pageSize = 10;
+
+    const city = searchParams.get("city") || undefined;
+    const propertyType = searchParams.get("propertyType") || undefined;
+    const status = searchParams.get("status") || undefined;
+    const timeline = searchParams.get("timeline") || undefined;
+    const search = searchParams.get("search") || undefined;
+    const sort = searchParams.get("sort") || "desc";
+
+    const where: { [key: string]: unknown } = {};
+
+    if (city) where.city = city;
+    if (propertyType) where.propertyType = propertyType;
+    if (status) where.status = status;
+    if (timeline) where.timeline = timeline;
+    if (search) {
+      where.OR = [
+        { fullName: { contains: search, mode: "insensitive" } },
+        { phone: { contains: search } },
+        { email: { contains: search, mode: "insensitive" } },
+      ];
+    }
+
+    const [buyers, total] = await Promise.all([
+      prisma.buyer.findMany({
+        where,
+        include: {
+          owner: { select: { name: true } },
+        },
+        orderBy: { updatedAt: sort === "asc" ? "asc" : "desc" },
+        skip: (page - 1) * pageSize,
+        take: pageSize,
+      }),
+      prisma.buyer.count({ where }),
+    ]);
+
+    return NextResponse.json(
+      {
+        data: buyers,
+        total,
+        page,
+        pageSize,
+      },
+      { status: 200 }
+    );
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ status: 500, error: "Server error" });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+    if (!session?.user?.id) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const userId = session.user.id;
+    const { data } = await req.json();
+    data["ownerId"] = userId;
+    const parsed = BuyerSchema.safeParse(data);
+
+    if (!parsed.success) {
+      return NextResponse.json({ status: 400, errors: parsed.error.issues });
+    }
+
+    const buyerData: BuyerType = parsed.data;
+
+    if (buyerData.budgetMin && buyerData.budgetMax) {
+      if (buyerData.budgetMax <= buyerData.budgetMin) {
+        return NextResponse.json({
+          status: 400,
+          message: "Max budget should be greater than min budget",
+        });
+      }
+    }
+
+    if (buyerData?.propertyType === "Apartment" && !buyerData?.bhk) {
+      return NextResponse.json({
+        status: 400,
+        error: "BHK is required for Apartment",
+      });
+    }
+
+    const buyer = await prisma.buyer.create({
+      data: {
+        ...(buyerData as BuyerType),
+      },
+    });
+
+    await prisma.buyerHistory.create({
+      data: {
+        buyerId: buyer.id,
+        changedBy: userId,
+        diff: { created: buyer },
+      },
+    });
+
+    return NextResponse.json({ status: 200, buyer });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ status: 500, error: "Server error" });
+  }
+}

3d8071d5694b83da1cdf9c869ab02e27f30709d5
Vansh <vanshmorwani1@gmail.com>
2025-09-15 21:18:07 +0530

    added components

---
+++ components/ui/logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <h1 className="text-3xl font-bold">
+      <span className="text-blue-600">e</span>
+      <span className="text-gray-800">Leads</span>
+    </h1>
+  );
+}

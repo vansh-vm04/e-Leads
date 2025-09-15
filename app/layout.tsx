1c05591c00c9ef17331979211f395b41b734e539
Vansh <vanshmorwani1@gmail.com>
2025-09-15 21:05:32 +0530

    added initial session check to redirect on login or dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Providers from "@/lib/Providers";
+import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>{children}</Providers>
+        <ToastContainer position="top-center" />
       </body>
     </html>
   );
---
+++ components/PageLoader.tsx
@@ -0,0 +1,9 @@
+const PageLoader = () => {
+  return (
+    <div className="flex bg-white fixed z-50 items-center justify-center h-screen w-screen">
+        <div className="loader"></div>
+    </div>
+  )
+}
+
+export default PageLoader
